f81204027664a134b798da1a8536e31333a6ebe1
goyo123321 <182012007+goyo123321@users.noreply.github.com>
2025-10-24 15:45:20 +0800

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -136,38 +136,4 @@ const workerConfig = {
 }
 
 // Don't forget this, otherwise compilation fails.
-export { pageConfig, workerConfig }    // [Optional] grace period in minutes before sending a notification
-    // notification will be sent only if the monitor is down for N continuous checks after the initial failure
-    // if not specified, notification will be sent immediately
-    gracePeriod: 5,
-  },
-  callbacks: {
-    onStatusChange: async (
-      env: any,
-      monitor: any,
-      isUp: boolean,
-      timeIncidentStart: number,
-      timeNow: number,
-      reason: string
-    ) => {
-      // This callback will be called when there's a status change for any monitor
-      // Write any Typescript code here
-
-      // This will not follow the grace period settings and will be called immediately when the status changes
-      // You need to handle the grace period manually if you want to implement it
-    },
-    onIncident: async (
-      env: any,
-      monitor: any,
-      timeIncidentStart: number,
-      timeNow: number,
-      reason: string
-    ) => {
-      // This callback will be called EVERY 1 MINTUE if there's an on-going incident for any monitor
-      // Write any Typescript code here
-    },
-  },
-}
-
-// Don't forget this, otherwise compilation fails.
 export { pageConfig, workerConfig }
